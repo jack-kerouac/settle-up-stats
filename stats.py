f7b9509fd8292a610f5eed2ed8e6345aa452940b
Florian Rampp <Florian.Rampp@gmail.com>
2013-10-30 12:07:01 +0100

    code cleanup

--- stats.py
+++ stats.py
@@ -7,6 +7,7 @@ import sys
 import datetime
 import argparse
 
+# PARSE ARGS
 parser = argparse.ArgumentParser(description="Transforms exported SQLite database of the mobile app SettleUp "
                                              "into CSV for evaluating tracked expenses.")
 parser.add_argument('--sqlite-file', required=True,
@@ -15,15 +16,18 @@ parser.add_argument('--group-name', required=True,
                     help='the name of the SettleUp group that shall be regarded')
 parser.add_argument('--currency', required=True,
                     help='the currency code for the monetary amounts in the output CSV')
-
 args = parser.parse_args()
 
 groupName = args.group_name
 
 outputCurrency = args.currency
 
+
+# OPEN SQLITE FILE
 conn = sqlite3.connect(args.sqlite_file.name)
+# be able to access rows as dictionaries
 conn.row_factory = sqlite3.Row
+
 
 # GET CURRENCIES
 mcu = conn.cursor()
@@ -62,38 +66,38 @@ for m in mc:
 if not members:
     raise argparse.ArgumentError('no members for group ' + groupName)
 
-# CSV HEADERS
 
-#with open('payments.csv', 'wb') as csvFile:
+# WRITE CSV HEADERS
 writer = csv.writer(sys.stdout, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-writer.writerow(['purpose', 'date', 'full amount ({})'.format(outputCurrency)] + list(members.values()))
+writer.writerow(['purpose', 'date', 'full amount ({})'.format(outputCurrency)] +
+                list(map(lambda x: '{} ({})'.format(x, outputCurrency), list(members.values()))))
 
 
-# GET PAYMENTS
+# FOR EACH PAYMENT, TRANSFORM IT, AND WRITE CSV LINE
 pc = conn.cursor()
 
-pc.execute('''
+pc.execute("""
 SELECT p._id, p.for_who, p.amount, p.weights, p.purpose, p.created, p.currency, p.transfer
 FROM payments AS p INNER JOIN groups AS g ON p.group_id = g._id
-WHERE g.name = ?''', [groupName])
+WHERE g.name = ?""", [groupName])
 
 for payment in pc:
     # omit transfers, they are not expenses
     if payment['transfer'] == 1:
-        #print(payment['_id'], "TRANSFER")
         continue
-
-    howMuchForWho = {m: 0.0 for m in members.keys()}
 
     purpose = payment['purpose']
 
-    date = datetime.datetime.fromtimestamp(int(payment['created'])/1000).strftime('%Y-%m-%d %H:%M:%S')
+    date = datetime.datetime.fromtimestamp(int(payment['created']) / 1000).strftime('%Y-%m-%d %H:%M:%S')
 
-    forWho = [int(i) for i in payment['for_who'].split(' ')]
-    weights = [float(i) for i in payment['weights'].split(' ')]
     # sum amount since multiple people could have paid
     amount = sum([float(i) for i in payment['amount'].split(' ')])
     amountInOutputCurrency = amount / exchangeRateInOutputCurrency[payment['currency']]
+
+    forWho = [int(i) for i in payment['for_who'].split(' ')]
+    weights = [float(i) for i in payment['weights'].split(' ')]
+
+    howMuchForWho = {m: 0.0 for m in members.keys()}
 
     for i, benefitor in enumerate(forWho):
         howMuchForWho[benefitor] = weights[i] / sum(weights) * amountInOutputCurrency
